fb6f642e6bbcd62eece9ff172713af706f198e01
Yu-En Wang <ernestwang100@gmail.com>
2024-03-26 20:54:31 -0700

    alter to QuizQuestionsEditor in Editor/index.tsx

--- src/Kanbas/Courses/Quizzes/Editor/index.tsx
+++ src/Kanbas/Courses/Quizzes/Editor/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import QuestionList from "../Questions/List";
+import QuizQuestionsEditor from "./QuestionsEditor";
 
 export default function Questions() {
   return (
     <div>
       <h2>Questions</h2>
-      <QuestionList />
+      <QuizQuestionsEditor />
     </div>
   );
 }
