da4db00ef85500f15f577674376e0deeaa473fa6
Yu-En Wang <ernestwang100@gmail.com>
2024-03-26 19:28:43 -0700

    Restore folder NewQuiz from commit e8c8e3eb4463fd466e0a97676be8cb3842cfe9af

---
+++ src/Kanbas/Courses/Quizzes/NewQuiz/NewQuizQuestions.tsx
@@ -0,0 +1,122 @@
+import { FaBan, FaSearch, FaEllipsisV, FaPlus } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import NewQuizNav from "./NewQuizNav";
+import QuizQuestionCard from "./QuizQuestionCard";
+
+export interface Question {
+  _id: string;
+  title: string;
+  body: string;
+  points: number;
+  type: string;
+}
+
+// default quesionslist should be empty, keep this line, placeholder
+// const questionsList: Question[] = [];
+
+const exampleQuestion1: Question = {
+  _id: "q111",
+  title: "Question 1",
+  body: "What is HTML?",
+  points: 5,
+  type: "fill-in-blank",
+};
+
+const exampleQuestion2: Question = {
+  _id: "q112",
+  title: "Question 2",
+  body: "What does DOM stand for?",
+  points: 10,
+  type: "fill-in-blank",
+};
+
+const questionsList: Question[] = [exampleQuestion1, exampleQuestion2];
+
+const totalPoints = questionsList.reduce(
+  (sum, question) => sum + question.points,
+  0
+);
+
+function NewQuizQuestions() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <div className="d-flex justify-content-end align-items-center text-center">
+        <span>Points {totalPoints}</span>
+        <span className="ms-3">
+          <FaBan /> Not Published
+        </span>
+        <button className="btn border ms-3">
+          <FaEllipsisV className="ms-1" />
+        </button>
+      </div>
+      <hr />
+
+      <NewQuizNav />
+
+      <div>
+        {questionsList.length === 0 ? (
+          <div style={{ height: "10em" }}></div>
+        ) : (
+          <ul>
+            {questionsList.map((question: Question) => (
+              <QuizQuestionCard key={question._id} question={question} />
+            ))}
+          </ul>
+        )}
+      </div>
+
+      <div className="d-flex justify-content-center gap-5">
+        <button
+          className="btn border d-flex align-items-center"
+          onClick={() =>
+            navigate(`/Kanbas/Courses/1/Quizzes/New/Questions/Multi-Choice`)
+          }
+        >
+          <FaPlus className="me-2" />
+          New Question
+        </button>
+        <button className="btn border d-flex align-items-center">
+          <FaPlus className="me-2" />
+          New Question Group
+        </button>
+        <button className="btn border d-flex align-items-center">
+          <FaSearch className="me-2" />
+          Questions
+        </button>
+      </div>
+      <hr />
+
+      <div className="d-flex align-items-center justify-content-between">
+        <label htmlFor="notifyUsers" className="">
+          <input type="checkbox" id="notifyUsers" name="notifyUsers" /> Notify
+          users this quiz has changed
+        </label>
+        <div>
+          <button
+            className="btn border ms-2"
+            onClick={() => navigate(`/Kanbas/Courses/1/Quizzes`)}
+          >
+            Cancel
+          </button>
+          <button
+            className="btn border ms-2"
+            onClick={() => navigate(`/Kanbas/Courses/1/Quizzes`)}
+          >
+            Save & Publish
+          </button>
+          <button
+            className="btn btn-danger ms-2"
+            onClick={() => navigate(`/Kanbas/Courses/1/Quizzes`)}
+          >
+            Save
+          </button>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
+}
+
+export default NewQuizQuestions;
---
+++ src/Kanbas/Courses/Quizzes/NewQuiz/QuizQuestionCard.tsx
@@ -0,0 +1,22 @@
+import { Question } from "./NewQuizQuestions";
+
+function QuizQuestionCard({ question }: { question: Question }) {
+  return (
+    <div className="container my-4">
+      <div className="card">
+        <div className="card-header d-flex justify-content-between align-items-center">
+          <span className="d-flex align-items-center">
+            <i className="fas fa-ellipsis-v me-2"></i>
+            {question.title}
+          </span>
+          <span>pts: {question.points}</span>
+        </div>
+        <div className="card-body">
+          <p className="card-text">{question.body}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default QuizQuestionCard;
